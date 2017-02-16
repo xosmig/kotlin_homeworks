a4384381bd4110cbcdba03aaf9164e0c5f1b6c95
Andrey Tonkikh <xosmig@gmail.com>
2017-02-17 01:10:30 +0300

    lazy added

---
+++ lazy/src/main/kotlin/com/xosmig/lazy/Lazy.kt
@@ -0,0 +1,124 @@
+package com.xosmig.lazy
+
+import java.util.concurrent.atomic.AtomicReference
+
+/**
+ * An interface for lazy evaluations.
+ * Also contains some factory static methods.
+ */
+interface Lazy<out T> {
+    /**
+     * Returns the result of the evaluation (computes if necessary).
+     * Should always return the same result.
+     *
+     * @return the result of the evaluation.
+     */
+    fun get(): T
+
+    companion object {
+        /**
+         * Wraps a single value into [Lazy].
+         *
+         * @param[value] value to return from the [get] method.
+         * @return [Lazy] which always returns [value] from the [get] method.
+         */
+        fun<T> value(value: T): Lazy<T> = LazyValue(value)
+
+        /**
+         * Wraps [expression] into thread-unsafe [Lazy].
+         *
+         * In the case of only one thread calls `expression.get()`
+         * only once at the moment of the first [get] call.
+         * [get] method returns the result of the first `expression.get()` call.
+         * The behaviour is undefined in the case of concurrent operations.
+         *
+         * @param[expression] an expression for lazy evaluation
+         * @return [expression] wrapped into thread-unsafe [Lazy].
+         */
+        fun<T> threadUnsafe(expression: () -> T): Lazy<T> = ThreadUnsafeLazyExpr(expression)
+
+        /**
+         * Wraps [expression] into thread-safe [Lazy].
+         *
+         * Calls `expression.get()` only once at the moment of the first [get] call.
+         * [get] method returns the result of the first `expression.get()` call.
+         *
+         * @param[expression] an expression for lazy evaluation
+         * @return [expression] wrapped into thread-safe [Lazy].
+         */
+        fun<T> threadSafe(expression: () -> T): Lazy<T> = ThreadSafeLazyExpr(expression)
+
+        /**
+         * Wraps [expression] into lock-free thread-safe [Lazy].
+         *
+         * May call `expression.get()` multiple times concurrently, but only when [get] called.
+         * [get] always returns the same result,
+         * but it's not necessary the result of the first `expression.get()` call.
+         *
+         * @param[expression] an expression for lazy evaluation
+         * @return [expression] wrapped into lock-free thread-safe [Lazy].
+         */
+        fun<T> lockFree(expression: () -> T): Lazy<T> = LockFreeLazyExpr(expression)
+    }
+}
+
+private class LazyValue<out T>(private val value: T) : Lazy<T> {
+    override fun get(): T {
+        return value
+    }
+}
+
+private class ThreadUnsafeLazyExpr<out T>(expression: () -> T) : Lazy<T> {
+    private var expression: (() -> T)? = expression
+    private var result: T? = null
+
+    override fun get(): T {
+        val expression = this.expression
+        if (expression != null) {
+            result = expression()
+            this.expression = null
+        }
+        return result as T
+    }
+}
+
+private class ThreadSafeLazyExpr<out T>(expression: () -> T) : Lazy<T> {
+    object NOTHING  // singleton
+
+    private var result: Any? = NOTHING
+    private var expression: (() -> T)? = expression
+
+    override fun get(): T {
+        if (result == NOTHING) {  // to avoid unnecessary locks
+            synchronized(this) {
+                if (result == NOTHING) {
+                    result = expression!!.invoke()
+                    expression = null
+                }
+            }
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        return result as T
+    }
+}
+
+private class LockFreeLazyExpr<out T>(expression: () -> T) : Lazy<T> {
+    companion object {
+        val NOTHING = Any()
+    }
+
+    private var result: AtomicReference<Any?> = AtomicReference(NOTHING)
+    @Volatile private var expression: (() -> T)? = expression
+
+    override fun get(): T {
+        val expression = this.expression
+        if (expression != null) {  // checking for `result.get() == NOTHING` would be redundant
+            result.compareAndSet(NOTHING, expression())
+            this.expression = null
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        return result.get() as T
+    }
+}
